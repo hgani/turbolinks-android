f03ac1b6257047d901cdd59c009b0961b8afa977
Hendrik <hendrik.gani@gmail.com>
2016-11-01 22:47:34 +1100

    restore bug fix

--- turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksSessionWrapper.java
+++ turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksSessionWrapper.java
@@ -31,8 +31,16 @@ public class TurbolinksSessionWrapper {
 
     // This is mainly to prevent issue with file upload whereby the webview gets refreshed and losing its current state after selecting a file.
     // Another positive side effect is performance.
+    //
+    // NOTE: In the future, in the unlikely event that we want to open a new TL activity from a TL form, we'd need a more generic solution
+    // that saves and restores the webview state so user won't lose their input. But this is quite unlikely and preliminary attempt shows
+    // that it's not straight forward. In addition it also seems to have a noticeable performance impact.
     if (session.webViewAttachedToNewParent) {
       session.visit(url);
+    }
+    else {
+      // Reset state
+      session.restoreWithCachedSnapshot = false;
     }
   }
 
